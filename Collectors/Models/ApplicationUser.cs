0743b7c25225610917102ef1343fb8a49ea77e03
Allison Patton <allison.tpttn@gmail.com>
2019-11-22 15:18:29 -0600

    able to create collections and collectibles

---
+++ Collectors/Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Collectors.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(25)]
+        [MinLength(1)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(35)]
+        [MinLength(1)]
+        public string LastName { get; set; }
+        public List<Collection> Collections { get; set; }
+    }
+}
---
+++ Collectors/Models/Collectible.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Collectors.Models
+{
+    public class Collectible
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(55)]
+        [MinLength(1)]
+        public string Name { get; set; }
+
+        [MaxLength(4000)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CollectedDate { get; set; }
+        public int  CollectionId { get; set; }
+        public Collection Collection { get; set; }
+    }
+}
